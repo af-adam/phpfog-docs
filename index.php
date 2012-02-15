49b53457c32efe40dacea020d4ee8e4b2841d1fa
Maciej Skierkowski <maciej@skierkowski.com>
2012-02-15 15:09:42 -0800

    another redirect

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	{
 		$url="https://phpfog.com/jumpstarts";
 	}
+	if($_SERVER["PATH_INFO"]=="/features/article/getting_started_popular_application")
+	{
+		$url="http://dev.appfog.com/features/article/getting_started_custom_application";
+	}
 	else
 	{
 		$url="http://dev.appfog.com" . $_SERVER["PATH_INFO"];
