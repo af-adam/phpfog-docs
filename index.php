d082e8c00565322a3ef8efe7a6f0ca501157164c
Maciej Skierkowski <maciej@skierkowski.com>
2012-02-15 15:01:29 -0800

    special case one directory

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-	$url="http://dev.appfog.com" . $_SERVER["PATH_INFO"];
+	$url="";
+	if($_SERVER["PATH_INFO"]=="/features/article/supported_frameworks")
+	{
+		$url="https://phpfog.com/jumpstarts";
+	}
+	else
+	{
+		$url="http://dev.appfog.com" . $_SERVER["PATH_INFO"];
+	}
   	header ('HTTP/1.1 301 Moved Permanently');
   	header('Location:' . $url);
 	
