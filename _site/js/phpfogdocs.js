3457d0754cc8b5155be53dbdd2e0886350161700
Joe <joe@appfog.com>
2012-06-25 20:02:59 -0700

    remove top-link fix accordion

--- _site/js/phpfogdocs.js
+++ _site/js/phpfogdocs.js
@@ -1,11 +1,5 @@
 $(function() {
 
-	var	$topLink	= $("#top-link"), 
-		$window		= $(window),
-		offset		= $topLink.offset(),
-		winHeight	= $(window).height(),
-		docHeight 	= $(document.body).height();
-
 	$('a.accordion-toggle').bind('click',function() {
 		if ( $(this).find('i.accordion-icon').hasClass('icon-chevron-right') ) {
 			$('.accordion-icon').removeClass('icon-chevron-down');
@@ -13,19 +7,6 @@ $(function() {
 			$(this).find('i').toggleClass('icon-chevron-down icon-chevron-right');
 		} else {
 			$(this).find('i').toggleClass('icon-chevron-down icon-chevron-right');
-		}
-	});
-
-	$window.scroll(function() {
-		if ( $window.scrollTop() < offset.top - winHeight + 52 ) {
-			$topLink.removeClass('fixed');
-			$topLink.removeClass('tbottom');
-		} else if ( $window.scrollTop() < ( $(document.body).height() - ( winHeight + 201 ) ) ) {
-			$topLink.removeClass('tbottom');
-			$topLink.addClass('fixed');
-		} else {
-			$topLink.removeClass('fixed');
-			$topLink.addClass('tbottom');
 		}
 	});
 
